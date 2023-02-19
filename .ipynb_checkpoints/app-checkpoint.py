f81c0eed8d1ddf14075306e4fa533afa1eb523c6
haominmai <haominmai@gmail.com>
2023-02-19 01:52:16 -0800

    create database and some little change

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
+    
     return render_template("base.html")
 
 
@@ -15,20 +16,14 @@ def submit():
         return render_template("submit.html")
     
     else: 
-        
-        print(request.form['message'], request.form['name'])
-        
-        return render_template("view.html",
-                               message = request.form['message'],
-                               name = request.form['name']
-                               ) 
-
-@app.route('/view/')
-def view():
-    return render_template("view.html")
+        name, message = insert_message(request)
+                
+        return render_template('submit.html', message = message, name = name, submit = True)
 
 
 import sqlite3
+
+from flask import g
 
 
 def get_message_db():
@@ -37,24 +32,92 @@ def get_message_db():
     """
     try:
         
-        #check if there is a database called message_db in the g attribute of the app
+        # check if there is a database called message_db in the g attribute of the app
         return g.message_db
     
     except:
         
-        #if there is no database, connect that database
+        # if there is no database, connect that database
         g.message_db = sqlite3.connect("messages_db.sqlite")
         
         # create table called message if it's not exists in database
-        # in this table, we have three columns: id, handle, message
-        cmd = 'CREATE TABLE IF NOT EXISTS messages (id integer, handle text, message text)' 
+        # in this table, we have three columns: handle, message
+        cmd = \
+        """
+        CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        message TEXT) 
         
+        """
         # get a cursor object
         cursor = g.message_db.cursor()
         
-        #Execute sql command
+        # execute sql command
         cursor.execute(cmd)
         
         return g.message_db
 
 
+def insert_message(request):
+    """
+    Extract message and handle from request, 
+    and save user message into database
+    """
+    
+    # extract message and name from request
+    message = request.form['message']
+    name = request.form['name']
+
+    # open connection to database
+    conn = get_message_db()
+    cursor = conn.cursor()
+    
+    # insert into our database
+    cmd = \
+    f"""
+    INSERT INTO messages (name, message) 
+    VALUES ('{name}', '{message}')
+    """
+    
+    cursor.execute(cmd)
+    
+    # ensure our row insertion has been saved
+    conn.commit()
+    
+    # close the database connection 
+    conn.close()
+    
+    return name, message
+
+
+def random_messages(n):
+    """
+    Return a collection of n random messages
+    from the message_db database
+    """
+    
+    # open connection to database
+    conn = get_message_db()
+    cursor = conn.cursor()
+    
+    # select message from database
+    cmd = \
+    f"""
+    SELECT name ,message FROM messages ORDER BY RANDOM() LIMIT {n}
+    """
+
+    # get the message
+    cursor.execute(cmd)
+    messages = cursor.fetchall()
+    
+    # close the database connection
+    conn.close()
+
+    return  messages
+
+
+@app.route('/view/')
+def view():
+    
+    return render_template('view.html', messages = random_messages(5))
